22815dfdfe8bb988398fadf0afc285b41591ddc2
East1036 <tunjing998@gmail.com>
2020-11-17 18:51:53 +0000

    Setup Inital Test Testing

--- Aquality2/Aquality2/settings.py
+++ Aquality2/Aquality2/settings.py
@@ -81,7 +81,8 @@ DATABASES = {
     'USER': 'root',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
-    'PORT': '3306'
+    'PORT': '3306',
+    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
   }
   }
 
