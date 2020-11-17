0dda1962adf153ce65ed1557f22e2b0e1ae34f6a
East1036 <tunjing998@gmail.com>
2020-11-17 16:02:16 +0000

    Initial Setup Database

--- Aquality2/Aquality2/settings.py
+++ Aquality2/Aquality2/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'Aquality2.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': ***REMOVED***,
+    'USER': ***REMOVED***,
+    'PASSWORD': '',
+    'HOST': ***REMOVED***,
+    'PORT': ***REMOVED***
+  }
+  }
 
 
 # Password validation
