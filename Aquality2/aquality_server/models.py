8e30093de5df649e2e73d43abe85ec20c4b0980f
East1036 <tunjing998@gmail.com>
2020-11-17 15:02:40 +0000

    Initial User Model

--- Aquality2/aquality_server/models.py
+++ Aquality2/aquality_server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Login_Account(models.Model):
+    account_id = models.AutoField(primary_key = True)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+
