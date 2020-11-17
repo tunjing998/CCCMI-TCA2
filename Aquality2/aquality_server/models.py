dac37b0697b2a468f0391b117d9dbb70dc247ec9
East1036 <tunjing998@gmail.com>
2020-11-17 15:07:19 +0000

    Initial UserAccount, Data, Image Model

--- Aquality2/aquality_server/models.py
+++ Aquality2/aquality_server/models.py
@@ -8,3 +8,22 @@ class Login_Account(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=200)
 
+class User_Account(models.Model):
+    user_id = models.OneToOneField(
+        Login_Account,
+        on_delete=models.CASCADE,
+        primary_key = True,
+    )
+    full_name = models.CharField(max_length = 200)
+    created_at = models.TimeField()
+    user_group = models.CharField(max_length = 200)
+
+class Image(models.Model):
+    image_id = models.AutoField(primary_key = True)
+    image_path = models.CharField(max_length =200)
+    data_id = models.ForeignKey(Data,on_delete=models.CASCADE)
+
+class Data(models.Model):
+    data_id = models.AutoField(primary_key = True)
+
+
