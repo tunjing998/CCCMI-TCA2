cc1334611cede74aca05f108cdc564a801652eaa
Wenyu Shi <d00198805@student.dkit.ie>
2020-11-21 22:25:39 +0000

    add river details page into navigation_template

--- mobile_ui/screens/ChooseRiver.js
+++ mobile_ui/screens/ChooseRiver.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
+import {Text, Button, StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import {ScreenContainer} from '../styles/screenContainer';
+import RiverDetails from '../components/RiverDetails';
 
 export const ChooseRiverScreen = ({navigation}) => {
   return (
     <ScreenContainer>
-      <Text>Choose River Screen</Text>
+      <RiverDetails />
     </ScreenContainer>
   );
 };
