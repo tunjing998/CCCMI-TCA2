80a3e5cbe88eff8a803cbb1dc1a85fad75de1aba
East1036 <tunjing998@gmail.com>
2020-11-21 17:20:29 +0000

    Function For Get All Waterbody Detail

--- Aquality2/catchmentApiCatch/models.py
+++ Aquality2/catchmentApiCatch/models.py
@@ -6,20 +6,60 @@ class CatchmentCatcher:
 
     catchmentsUrl = "https://wfdapi.edenireland.ie/api/"
     catchmentApiKey = 'catchment/'
-    subcatchmentApiKey = 'subcatchment'
-    waterbodyApiKey = 'waterbody'
+    subcatchmentApiKey = '/subcatchment/'
+    waterbodyApiKey = 'waterbody/'
 
-    def getMainCatchment():
-        """
-        docstring
-        """
-        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.catchmentApiKey
+    def getJsonFrom(targetUrl):
         response = requests.get(targetUrl)
-        jsonData = response.json()
+        return response.json()
+
+    def getCatchmentList():
+        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.catchmentApiKey
+        jsonData = CatchmentCatcher.getJsonFrom(targetUrl)
         return jsonData['Catchments']
 
-    def getSubCatchment():
-        catchmentList = getMainCatchment()
+    def getCatchmentDetail(code):
+        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.catchmentApiKey + code
+        jsonData = CatchmentCatcher.getJsonFrom(targetUrl)
+        return jsonData
+
+    def getCatchmentDetailList():
+        catchmentList = CatchmentCatcher.getCatchmentList()
+        catchmentDataList = []
         for catchment in catchmentList:
-            print(catchment)
-        pass
+            targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.catchmentApiKey + catchment['Code']
+            catchmentDataList.append(CatchmentCatcher.getJsonFrom(targetUrl))
+        return catchmentDataList
+
+    def getSubCatchmentDetail(subcatchmentcode):
+        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.catchmentApiKey + subcatchmentcode.split('_')[0]+ CatchmentCatcher.subcatchmentApiKey + subcatchmentcode
+        jsonData = CatchmentCatcher.getJsonFrom(targetUrl)
+        return jsonData
+
+    def getWaterBodyDetail(waterbodycode):
+        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.waterbodyApiKey + waterbodycode
+        jsonData = CatchmentCatcher.getJsonFrom(targetUrl)
+        return jsonData
+
+    def getAllWaterBodyCode():
+        catchmentList = CatchmentCatcher.getCatchmentDetailList()
+        waterbodyCodeList = []
+        for catchment in catchmentList:
+            for subcatchment in catchment['Subcatchments']:
+                for waterbody in CatchmentCatcher.getSubCatchmentDetail(subcatchment['Code']).get("Waterbodies"):
+                    waterbodyCodeList.append(waterbody["Code"])
+        return waterbodyCodeList
+            
+    def getAllWaterBodyDetails():
+        waterbodyCodeList = CatchmentCatcher.getAllWaterBodyCode()
+        
+        waterbodyDetailList = []
+        for waterbodyCode in waterbodyCodeList:
+            targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.waterbodyApiKey + waterbodyCode
+            waterbodyDetailList.append(CatchmentCatcher.getJsonFrom(targetUrl))
+        return waterbodyDetailList
+
+    def getOneWaterDetails():
+        waterbodyCode = "IE_NW_01B010100"
+        targetUrl = CatchmentCatcher.catchmentsUrl + CatchmentCatcher.waterbodyApiKey + waterbodyCode
+        return CatchmentCatcher.getJsonFrom(targetUrl)
--- Aquality2/catchmentApiCatch/views.py
+++ Aquality2/catchmentApiCatch/views.py
@@ -4,7 +4,9 @@ import requests
 from django.http import HttpResponse
 
 def home(request):
-    catchmentList = CatchmentCatcher.getMainCatchment()
+    # catchmentList = CatchmentCatcher.getMainCatchment()
+    catchmentList = CatchmentCatcher.getOneWaterDetails()
+    catchmentList.get("Catchment")
     return render(request, 'catchmentApiCatch/home.html', {
         'catchmentList': catchmentList
     })
