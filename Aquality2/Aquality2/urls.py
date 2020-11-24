023143692c854c286c14e7200a4f2594e96c4357
East1036 <tunjing998@gmail.com>
2020-11-24 16:05:57 +0000

    Remove Unused Url

--- Aquality2/Aquality2/urls.py
+++ Aquality2/Aquality2/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('aquality_server/', include('aquality_server.urls')),
-    path('admin/', admin.site.urls),
-    path('catchmentApiCatch/',include('catchmentApiCatch.urls'))
+    path('admin/', admin.site.urls)
 ]
