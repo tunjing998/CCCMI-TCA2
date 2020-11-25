71e1b5c43511176820393fb1a234433718417dfa
East1036 <tunjing998@gmail.com>
2020-11-25 15:46:31 +0000

    Done Simple Setup API for all River

--- Aquality2/aquality_server/urls.py
+++ Aquality2/aquality_server/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
-
+from django.urls import include,path
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'rivers', views.RiverViewSet)
 
 #Application Name
 app_name = 'aquality_server'
 #Controlling The Path of Application
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('addData', views.addData, name='addData')
+# ]
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('addData', views.addData, name='addData')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- Aquality2/aquality_server/views.py
+++ Aquality2/aquality_server/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from aquality_server.actions import *
+from rest_framework import viewsets
+from .serializers import RiverSerializer
+from .models import River
 
 # Controlling The View Access To
 def index(request):
     return render(request, 'aquality_server/index.html', {})
+
+class RiverViewSet(viewsets.ModelViewSet):
+    queryset = River.objects.all().order_by('river_id')
+    serializer_class = RiverSerializer
 
 # def addData(request):
 #     returnMessage = saveRiverListToDbFromWFA()
