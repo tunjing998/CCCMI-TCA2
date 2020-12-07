21226e1c91d28bc2e2438c8c23ba12bb4b5a15a6
Patrick <patricklee0221@gmail.com>
2020-12-07 13:28:09 +0000

    dark theme fix

--- mobile_ui/screens/ArduinoScreen.js
+++ mobile_ui/screens/ArduinoScreen.js
@@ -2,17 +2,48 @@ import React from 'react';
 import {View, Button, StyleSheet, TextInput} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Text} from 'react-native-elements';
+import {useTheme} from '@react-navigation/native';
 
 const ArduinoScreen = () => {
+  const {colors} = useTheme();
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      paddingBottom: 70,
+      color: colors.text,
+    },
+    searchSection: {
+      // flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      marginBottom: 30,
+    },
+    input: {
+      flex: 1,
+      paddingTop: 10,
+      marginLeft: 10,
+      paddingBottom: 10,
+      paddingLeft: 0,
+      backgroundColor: '#fff',
+      color: '#424242',
+      borderBottomColor: colors.text,
+      borderBottomWidth: 1,
+    },
+  });
   return (
     <View style={styles.container}>
-      {/* <Text>Arduino Screen</Text>
-      <Button title="Click Here" onPress={() => alert('Button Clicked!')} /> */}
       <Text h4 h4Style={styles.title}>
         Connect to Arduino.
       </Text>
       <View style={styles.searchSection}>
-        <TextInput placeholder="Arduino ID" style={styles.input} />
+        <TextInput placeholder="Insert Arduino ID here." style={styles.input} />
         <Icon.Button
           style={styles.searchIcon}
           name="magnify"
@@ -26,34 +57,3 @@ const ArduinoScreen = () => {
 };
 
 export default ArduinoScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    paddingBottom: 70,
-    // color: colors.text,
-  },
-  searchSection: {
-    // flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    marginBottom: 30,
-  },
-  input: {
-    flex: 1,
-    paddingTop: 10,
-    marginLeft: 10,
-    paddingBottom: 10,
-    paddingLeft: 0,
-    backgroundColor: '#fff',
-    color: '#424242',
-    // borderBottomColor: colors.text,
-    borderBottomWidth: 1,
-  },
-});
--- mobile_ui/screens/InsectScreen.js
+++ mobile_ui/screens/InsectScreen.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
 const InsectScreen = () => {
+  const {colors} = useTheme();
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      color: colors.text,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Insect Screen</Text>
+      <Text style={styles.title}>Insect Screen</Text>
       <Button title="Click Here" onPress={() => alert('Button Clicked!')} />
     </View>
   );
 };
 
 export default InsectScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
