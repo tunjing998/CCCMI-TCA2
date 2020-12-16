04dbb62187445ddd7c3dbc9ffbe494274d1a7a09
East1036 <tunjing998@gmail.com>
2020-12-16 20:25:04 +0000

    Modified the River APi Now Accpet GET Method
    modified the river api now accept get method
    the format will be
    url/aquality_server/rivers/?langitute=num&longitute=num

--- Aquality2/aquality_server/views.py
+++ Aquality2/aquality_server/views.py
@@ -5,6 +5,8 @@ from .serializers import RiverSerializer
 from .models import River
 from rest_framework.response import responses
 from rest_framework.decorators import api_view
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
 
 # Controlling The View Access To
 def index(request):
@@ -14,7 +16,12 @@ class RiverViewSet(viewsets.ModelViewSet):
     queryset = River.objects.all().order_by('river_id')
     serializer_class = RiverSerializer
     def get_queryset(self):
-        return River.objects.all().order_by('river_id')
+        # return River.objects.all().order_by('river_id')
+        if(self.request.query_params.get('longitute') and self.request.query_params.get('langitute')):
+            pnt =  GEOSGeometry('POINT(%s %s)' % (self.request.query_params.get('longitute'), self.request.query_params.get('langitute')))
+        else:
+            pnt = Point(-7.55,54.93)
+        return River.objects.filter(location__distance_lt=(pnt,D(m=10000)))
 
 
 # def addData(request):
@@ -22,4 +29,4 @@ class RiverViewSet(viewsets.ModelViewSet):
 #     return render(request, 'aquality_server/addData.html',{'returnMessage': returnMessage})
 
 def testingPage(request):
-    return render(request,'aquality_server/testing.html',{'request':request.path})
+    return render(request,'aquality_server/testing.html',{'request':request})
