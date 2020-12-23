0d274eab01d48b0ee09af7ed3697ae683ed91770
East1036 <tunjing998@gmail.com>
2020-12-23 22:21:22 +0000

    Done Filter Api By Search String Using Google API

--- Aquality2/aquality_server/filter.py
+++ Aquality2/aquality_server/filter.py
@@ -1,10 +1,17 @@
-import django_filters
-from .models import River
+from decouple import config
+import requests
+from django.contrib.gis.geos import Point
 
-class RiverFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='iexact')
 
-    class Meta:
-        model = River
-        fields = ['price', 'release_date']
-        
+def getLocationPoint(request):
+    if(request.query_params.get('longitute') and request.query_params.get('latitude')):
+        pnt = Point(request.query_params.get('longitute'),request.query_params.get('latitude'))
+    elif(request.query_params.get('location')):
+        request_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+ request.query_params.get('location')+"&inputtype=textquery&fields=geometry&key=" + config('GOOGLEMAP_APIKEY')
+        locationfound = requests.get(request_url)
+        data = locationfound.json().get("candidates")[0].get("geometry").get("location")
+        pnt = Point(data.get("lat"),data.get("lng"))
+    else:
+        pnt = Point(-7.55,54.93)
+    return pnt
+
