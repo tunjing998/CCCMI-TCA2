4a7b266c6de13d064fe747730a7d0c6a81670ec3
Patrick <patricklee0221@gmail.com>
2020-12-27 20:13:57 +0000

    test async storage

--- mobile_ui/screens/test03.js
+++ mobile_ui/screens/test03.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, Button, StyleSheet, Image} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const test03 = ({route}) => {
   const insects = route.params;
@@ -19,7 +20,43 @@ const test03 = ({route}) => {
     });
   };
 
-  return <ScrollView>{renderInsects()}</ScrollView>;
+  // TEST ASYNC STORAGE
+  const saveData = () => {
+    let obj = [
+      {
+        name: 'John Doe',
+        age: 5,
+        city: 'Stockholm',
+      },
+      {
+        name: 'Jane Doe',
+        age: 10,
+        city: 'Tokyo',
+      },
+    ];
+    AsyncStorage.setItem('user', JSON.stringify(obj));
+  };
+  const displayData = async () => {
+    try {
+      let user = await AsyncStorage.getItem('user');
+      let parse = JSON.parse(user);
+      alert(parse[1].city);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <ScrollView>
+      {renderInsects()}
+      <TouchableOpacity onPress={saveData()}>
+        <Text>Click me to save </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => displayData()}>
+        <Text>Click me to display </Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
