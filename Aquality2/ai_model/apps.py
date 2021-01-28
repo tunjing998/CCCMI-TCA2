d59e2f96f35f1fc6a3534bd4ecf2ff22bf695605
East1036 <tunjing998@gmail.com>
2021-01-28 14:19:20 +0000

    Removed Ai_model From Aquality Server

--- Aquality2/ai_model/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiModelConfig(AppConfig):
-    name = 'ai_model'
--- Aquality2/ai_model/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ai_model.views import object_detection_api
-
-# Controlling The Path of Application
-urlpatterns = [
-    path('ai_model/detect_image/', object_detection_api),
-]
