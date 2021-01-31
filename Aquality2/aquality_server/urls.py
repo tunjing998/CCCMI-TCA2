d84441cbfa0fa447c8f47ab9291d5a7b273ae220
IllyaMallon <56591546+IllyaMallon@users.noreply.github.com>
2021-01-31 23:17:12 +0000

    Pollution score calculation through POST
    Calculate score based on insect name and amount, gets post data from
    frontend and replies with json containing score

--- Aquality2/aquality_server/urls.py
+++ Aquality2/aquality_server/urls.py
@@ -1,30 +1,30 @@
-from django.urls import include,path
+from django.urls import include, path
 from rest_framework import routers
 from . import views
 from django.conf import settings
 
-
 router = routers.DefaultRouter()
 router.register(r'rivers', views.RiverViewSet)
-router.register(r'data',views.DataViewSet)
-router.register(r'insect',views.InsectViewSet)
-#Application Name
+router.register(r'data', views.DataViewSet)
+router.register(r'insect', views.InsectViewSet)
+# Application Name
 app_name = 'aquality_server'
-#Controlling The Path of Application
+# Controlling The Path of Application
 
 useraccount = [
-    path('useraccount/loginauth',views.checkUser,name='checkUser'),
-    path('useraccount/register',views.registerPage,name='registerPage'),
-    path('useraccount/delete',views.del_user,name='del_user'),
-    path('useraccount/checkname',views.if_username_exist,name='checkName'),
-    path('useraccount/checkemail',views.if_email_exist,name='checkEmail')
+    path('useraccount/loginauth', views.checkUser, name='checkUser'),
+    path('useraccount/register', views.registerPage, name='registerPage'),
+    path('useraccount/delete', views.del_user, name='del_user'),
+    path('useraccount/checkname', views.if_username_exist, name='checkName'),
+    path('useraccount/checkemail', views.if_email_exist, name='checkEmail'),
+    path('insect_score/score', views.calculate_score_insect, name='get_score'),
 ]
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-  #  path('addData', views.addData, name='addData'),
-    path('testing/',views.testingPage,name='testingPage'),
-    path('testingPageForPatrick',views.testingPageForPatrick,name='testingPageForPatrick'),
+                  path('', include(router.urls)),
+                  path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                  #  path('addData', views.addData, name='addData'),
+                  path('testing/', views.testingPage, name='testingPage'),
+                  path('testingPageForPatrick', views.testingPageForPatrick, name='testingPageForPatrick'),
 
-] + useraccount
+              ] + useraccount
