3b79341270f58b71cb3462726f793c28acc9fbbf
Patrick <patricklee0221@gmail.com>
2021-02-03 11:06:48 +0000

    modal to insert amount

--- mobile_ui/screens/selectInsect1_old.js
+++ mobile_ui/screens/selectInsect1_old.js
@@ -12,7 +12,6 @@ import {Button} from 'react-native-elements';
 import {ScrollView, TextInput} from 'react-native-gesture-handler';
 import axios from 'axios';
 import {useTheme} from '@react-navigation/native';
-import {IconButton, Colors} from 'react-native-paper';
 
 const selectInsect1 = ({navigation}) => {
   const [insectList, setInsectList] = useState([]);
@@ -33,13 +32,18 @@ const selectInsect1 = ({navigation}) => {
   });
 
   const [selecTedAmount, setSelectedAmount] = useState('');
-  const [modalVisible, setModalVisible] = useState(false);
-  const [actionTriggered, setActionTriggered] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
 
   const handleAdd = insectName => {
-    
+    if (!selecTedAmount.trim()) {
+      alert('Please Enter amount');
+      return;
+    }
+    const newSelectedInsect = Object.assign({}, selectedInsect);
+    newSelectedInsect.insect_name = insectName;
+    newSelectedInsect.amount = selecTedAmount;
+    setSelectedInsect(newSelectedInsect);
+    console.log(newSelectedInsect);
+    handleUpdate(newSelectedInsect);
   };
 
   useEffect(() => {
@@ -60,12 +64,13 @@ const selectInsect1 = ({navigation}) => {
   const handleSubmit = () => {
     navigation.navigate('Insect');
   };
-
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.viewContainer}>
       <ScrollView>
         {insectList.map((item, key) => (
           <View key={key} style={styles.container}>
+            
             <Image
               style={styles.tinyLogo}
               source={{
@@ -81,12 +86,18 @@ const selectInsect1 = ({navigation}) => {
               }}>
               {item.insect_name}
             </Text>
+            <TextInput
+              ref={textInput}
+              placeholder="amount"
+              style={styles.input}
+              keyboardType="numeric"
+              onChangeText={val => {
+                setSelectedAmount(val);
+              }}
+            />
             <Button
               onPress={() => {
-                setModalVisible(true);
-                setActionTriggered(item.insect_name); // HERE
-                setDescription(item.insect_desc);
-                setImage(item.insect_image_path);
+                handleAdd(item.insect_name);
               }}
               title="add"
               buttonStyle={{
@@ -113,40 +124,6 @@ const selectInsect1 = ({navigation}) => {
         onPress={() => handleSubmit()}
         type="outline"
       />
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {}}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Image
-              style={styles.tinyLogo}
-              source={{
-                uri: image,
-              }}
-            />
-            <Text>{actionTriggered}</Text>
-            <Text>{description}</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-              <TextInput placeholder="amount" onChangeText={(val)=>setSelectedAmount(val)} keyboardType='numeric'/>
-              <IconButton
-                icon="check-circle"
-                color={Colors.green500}
-                size={20}
-                onPress={() => {handleAdd();setModalVisible(!modalVisible)}}
-              />
-            </View>
-
-            <IconButton
-              icon="close-circle"
-              color={Colors.red500}
-              size={20}
-              onPress={() => setModalVisible(!modalVisible)}
-            />
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 };
@@ -163,6 +140,7 @@ const styles = StyleSheet.create({
     height: 80,
   },
   container: {
+    // width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -171,36 +149,8 @@ const styles = StyleSheet.create({
     width: 100,
   },
   submitButton: {
+    // backgroundColor: '#009999',
     padding: 10,
-    borderWidth: 2,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    width: 300,
-    height: 'auto',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: 'red',
+    borderWidth: 3,
   },
 });
