e7fbf6715b54ada5201b41949f01de2820e2da65
Patrick <patricklee0221@gmail.com>
2021-02-04 13:51:50 +0000

    data -> date

--- mobile_ui/screens/HistoryDetail.js
+++ mobile_ui/screens/HistoryDetail.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { useTheme } from '@react-navigation/native';
-import { ListItem, Icon } from 'react-native-elements';
+import React, {Component} from 'react';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import {ListItem, Icon} from 'react-native-elements';
 
-const HistoryDetail = ({ route }) => {
-  const { colors } = useTheme();
-  const { item } = route.params;
+const HistoryDetail = ({route}) => {
+  const {colors} = useTheme();
+  const {item} = route.params;
 
   const styles = StyleSheet.create({
     container: {
@@ -27,54 +27,39 @@ const HistoryDetail = ({ route }) => {
         <ListItem.Content>
           <ListItem.Subtitle style={styles.title}>
             Sample Score
-            </ListItem.Subtitle>
-          <Text style={styles.title}>
-            {item.sample_score}
-          </Text>
+          </ListItem.Subtitle>
+          <Text style={styles.title}>{item.sample_score}</Text>
         </ListItem.Content>
       </ListItem>
       <ListItem bottomDivider containerStyle={styles.listContainer}>
         <ListItem.Content>
-          <ListItem.Subtitle style={styles.title}>
-            Data
-            </ListItem.Subtitle>
-          <Text style={styles.title}>
-            {item.newDate}
-          </Text>
+          <ListItem.Subtitle style={styles.title}>Date</ListItem.Subtitle>
+          <Text style={styles.title}>{item.newDate}</Text>
         </ListItem.Content>
       </ListItem>
       <ListItem bottomDivider containerStyle={styles.listContainer}>
         <ListItem.Content>
-          <ListItem.Subtitle style={styles.title}>
-            Water pH
-            </ListItem.Subtitle>
-          <Text style={styles.title}>
-            {item.sample_ph}
-          </Text>
+          <ListItem.Subtitle style={styles.title}>Water pH</ListItem.Subtitle>
+          <Text style={styles.title}>{item.sample_ph}</Text>
         </ListItem.Content>
       </ListItem>
       <ListItem bottomDivider containerStyle={styles.listContainer}>
         <ListItem.Content>
           <ListItem.Subtitle style={styles.title}>
             Water Temperature
-            </ListItem.Subtitle>
-          <Text style={styles.title}>
-            {item.sample_tmp}
-          </Text>
+          </ListItem.Subtitle>
+          <Text style={styles.title}>{item.sample_tmp}</Text>
         </ListItem.Content>
       </ListItem>
       <ListItem bottomDivider containerStyle={styles.listContainer}>
         <ListItem.Content>
           <ListItem.Subtitle style={styles.title}>
             Sample taken by
-            </ListItem.Subtitle>
-          <Text style={styles.title}>
-            {item.sample_user}
-          </Text>
+          </ListItem.Subtitle>
+          <Text style={styles.title}>{item.sample_user}</Text>
         </ListItem.Content>
       </ListItem>
     </View>
-
   );
 };
 
